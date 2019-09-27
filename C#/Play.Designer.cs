1f8cbd35dc8d151b99d4cf004a13c8f9154063f9
Hana <lecheln>
2019-09-27 22:21:53 +0900

    update C#

---
+++ C#/Play.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace bulls_and_cows
+{
+    partial class Play
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.output.Location = new System.Drawing.Point(128, 74);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(29, 27);
+            this.output.TabIndex = 12;
+            this.output.Text = "0";
+            this.output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(315, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 316);
+            this.listBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "숫자를 입력해 주세요";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(22, 285);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(269, 41);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Go";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(22, 107);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(269, 172);
+            this.flowLayoutPanel1.TabIndex = 14;
+            // 
+            // Play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 338);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Play";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "숫자야구";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ C#/Play.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bulls_and_cows
+{
+    public partial class Play : Form
+    {
+
+        public const int size = 3;
+        private string comnum = "";
+
+        public void Checknum(string num)
+        {
+            int strike = 0;
+            int ball = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (num[i] == comnum[j])
+                    {
+                        if (i == j) strike += 1;
+                        else ball += 1;
+                    }
+                }
+            }
+
+            if (ball == 0 && strike == 0)
+            {
+                label1.Text = "out";
+                listBox1.Items.Add(num + " : OUT");
+            }
+            else
+            {
+                if (strike == size)
+                {
+                    label1.Text = "정답입니다.";
+                    MessageBox.Show("정답입니다.");
+                }
+                else
+                {
+                    label1.Text = "S : " + strike + " B : " + ball;
+                    listBox1.Items.Add(num + " : " + label1.Text);
+                }
+            }
+        }
+
+
+
+        public Play()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                Button button = new Button()
+                {
+                    Text = i.ToString(),
+                };
+                button.Click += new EventHandler(Btn_Click);
+                flowLayoutPanel1.Controls.Add(button);
+
+            }
+
+
+            Random random = new Random();
+
+            string tmpstr = "0123456789";
+            while (comnum.Length < 3)
+            {
+                int tmp = random.Next(0, tmpstr.Length); // 0 ~ 9
+                comnum += tmpstr[tmp];
+                tmpstr = tmpstr.Remove(tmp, 1);
+            }
+
+        }
+        
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            if (output.Text.Length == size)
+            {
+                for (int i = 0; i < size; i++) 
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (i == j) continue;
+                        if (output.Text[i] == output.Text[j])
+                        {
+                            MessageBox.Show("숫자가 중복됩니다.");
+                            output.Text = "0";
+                            return;
+                        }
+                    }
+                }
+                Checknum(output.Text);
+                output.Text = "0";
+            }
+            else
+            {
+                MessageBox.Show("숫자를 입력해주세요");
+            }
+        }
+        
+        void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string num = btn.Text;
+            if (output.Text == "0") output.Text = num;
+            else output.Text += num;
+            if (output.Text.Length == size + 1)
+            {
+                output.Text = output.Text.Remove(0, 1);
+            }
+        }
+    }
+}
